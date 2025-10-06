6dfa82a7548961972b19785986a369b0670e46ad
Joel NS <joel.silvap@gmail.com>
2025-10-06 20:47:45 -0300

    Fix indentation in test_get_account_not_found method

--- service/routes.py
+++ service/routes.py
@@ -91,10 +91,10 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
-def test_get_account_not_found(self):
+    def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
-        # send a self.client.get() request to the BASE_URL with an invalid account number (e.g., 0)
-        # assert that the resp.status_code is status.HTTP_404_NOT_FOUND
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
