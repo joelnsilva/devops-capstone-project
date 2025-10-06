c3c7191ff23d722f024b7153e6fb87cb8e52abd3
Joel NS <joel.silvap@gmail.com>
2025-10-06 20:56:02 -0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -94,7 +94,7 @@ def get_accounts(account_id):
 
 def test_get_account_not_found(self):
     """It should not Read an Account that is not found"""
-    resp = self.client.get(f"{BASE_URL}/0")
+    resp = self.client.get(f"/accounts/{id}/0")
     self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
 ######################################################################
