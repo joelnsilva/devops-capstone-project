941f7719a7b05c5c036e5d93666316e2eff99593
Joel NS <joel.silvap@gmail.com>
2025-10-06 22:07:44 -0300

    Remove unnecessary blank lines in routes.py

--- service/routes.py
+++ service/routes.py
@@ -68,10 +68,10 @@ def list_accounts():
     This endpoint will list all Accounts
     """
     app.logger.info("Request to list Accounts")
-    
+
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
-    
+
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
